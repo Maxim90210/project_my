a8457f56a33e086d48da60cd6f7b00404d6e05b9
Maxim90210 <150370357+Maxim90210@users.noreply.github.com>
2024-09-14 18:04:20 +0300

    Add files via upload

--- forms.py
+++ forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Ім\'я користувача', validators=[DataRequired(), Length(min=4, max=20)])
+    username = StringField('Імя користувача', validators=[DataRequired(), Length(min=4, max=20)])
     password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Зареєструватися')
 
 class LoginForm(FlaskForm):
-    username = StringField('Ім\'я користувача', validators=[DataRequired(), Length(min=4, max=20)])
+    username = StringField('Імя користувача', validators=[DataRequired(), Length(min=4, max=20)])
     password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Увійти')
